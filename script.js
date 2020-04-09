5f889cadcb025b80477457b55a93b09111394e03
sam11385 <samluedke@gmail.com>
2020-04-09 09:32:08 -0600

    Update mileage, remove console log, update package json details.

--- script.js
+++ script.js
@@ -1,68 +1,74 @@
- // Iniatialize tooltip with jquery (yuck)
- $(function() {
-     $('[data-toggle="tooltip"]').tooltip()
- })
- const leaseMileageAllowed = 30000;
- const daysInLease = 1100;
- const startingMileage = 136;
- const currentMiles = 1695;
- const dailyAllowance = (leaseMileageAllowed / daysInLease).toFixed(5);
+// Iniatialize tooltip with jquery (yuck)
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+const leaseMileageAllowed = 30000;
+const daysInLease = 1100;
+const startingMileage = 136;
+const currentMiles = 1698;
+const dailyAllowance = (leaseMileageAllowed / daysInLease).toFixed(5);
 
- console.log(dailyAllowance);
+// Get days for MY lease
+function getDays() {
+    const mileageDate = new Date(document.getElementById("mileageDate").value);
+    const startDate = new Date(document.getElementById("startDate").value);
+    return parseInt((mileageDate - startDate) / (24 * 3600 * 1000) + 1);
+}
 
- // Get days for MY lease
- function getDays() {
-     const mileageDate = new Date(document.getElementById("mileageDate").value);
-     const startDate = new Date(document.getElementById("startDate").value);
-     return parseInt((mileageDate - startDate) / (24 * 3600 * 1000) + 1);
- }
+// Calculate Miles for MY lease
+function calculateMiles() {
+    if (document.getElementById("mileageDate")) {
+        document.getElementById("numberOfDays").innerHTML = getDays();
+        const calcMiles = getDays() * dailyAllowance + startingMileage;
+        document.getElementById("calculatedMileage").innerHTML =
+            calcMiles.toFixed(2) + " miles";
+        document.getElementById("comparedMileage").innerHTML = (
+            calcMiles - currentMiles
+        ).toFixed(2);
 
- // Calculate Miles for MY lease
- function calculateMiles() {
-     if (document.getElementById("mileageDate")) {
-         document.getElementById("numberOfDays").innerHTML = getDays();
-         const calcMiles = getDays() * dailyAllowance + startingMileage;
-         document.getElementById("calculatedMileage").innerHTML = calcMiles.toFixed(2) + " miles";
-         document.getElementById("comparedMileage").innerHTML = ((calcMiles - currentMiles).toFixed(2));
+        if (calcMiles - currentMiles > 0) {
+            document.getElementById("underBy").classList.remove("hide");
+        } else if (calcMiles - currentMiles < 0) {
+            document.getElementById("overBy").classList.remove("hide");
+        }
+    }
+    toggleOverUnder();
+}
 
-         if (calcMiles - currentMiles > 0) {
-             document.getElementById("underBy").classList.remove("hide");
-         } else if (calcMiles - currentMiles < 0) {
-             document.getElementById("overBy").classList.remove("hide");
-         }
-     }
-     toggleOverUnder();
- }
+function toggleOverUnder() {
+    const showOverUnder = document.getElementById("showOverUnder");
+    showOverUnder.classList.remove("hide");
+}
 
- function toggleOverUnder() {
-     const showOverUnder = document.getElementById("showOverUnder");
-     showOverUnder.classList.remove("hide");
- }
+// Pass in my miles
+document.getElementById("currentMiles").innerHTML = currentMiles + " miles";
 
- // Pass in my miles
- document.getElementById("currentMiles").innerHTML = currentMiles + " miles";
+// User input functionality
+// Get days for ANY input dates
+function calculateDays() {
+    const userStartDate = new Date(
+        document.getElementById("userSelectStartDate").value
+    );
+    const userEndDate = new Date(
+        document.getElementById("userSelectEndDate").value
+    );
+    document.getElementById("totalDaysInlease").innerHTML = parseInt(
+        (userEndDate - userStartDate) / (24 * 3600 * 1000) + 1
+    );
+    toggleDivs();
+}
 
+function enableButton() {
+    if (!document.getElementById("userSelectStartDate").value ||
+        !document.getElementById("userSelectEndDate").value
+    ) {
+        document.getElementById("calculateButtton").disabled = true;
+    } else {
+        document.getElementById("calculateButtton").disabled = false;
+    }
+}
 
- // User input functionality
- // Get days for ANY input dates
- function calculateDays() {
-     const userStartDate = new Date(document.getElementById("userSelectStartDate").value);
-     const userEndDate = new Date(document.getElementById("userSelectEndDate").value);
-     document.getElementById('totalDaysInlease').innerHTML = parseInt((userEndDate - userStartDate) / (24 * 3600 * 1000) + 1);
-     toggleDivs();
- }
-
-
- function enableButton() {
-     if (!document.getElementById("userSelectStartDate").value || !document.getElementById("userSelectEndDate").value) {
-         document.getElementById('calculateButtton').disabled = true;
-     } else {
-         document.getElementById('calculateButtton').disabled = false;
-     }
- }
-
-
- function toggleDivs() {
-     const show = document.getElementById("showMe");
-     show.classList.toggle("hide");
- }
+function toggleDivs() {
+    const show = document.getElementById("showMe");
+    show.classList.toggle("hide");
+}
